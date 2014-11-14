fe22002ad5e0363ddf36926709ec6de9ec9b3f07
akeaswaran <Akshay Easwaran>
2014-11-13 23:29:54 -0500

    Added Isolate

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 ./remove.sh
 
-dpkg-deb -b -Zgzip PHPrivacyFlipswitch
-dpkg-deb -b -Zgzip PriorityHub
-dpkg-deb -b -Zgzip Titler
+dpkg-deb -b -Zgzip Isolate
 
 dpkg-scanpackages -m . /dev/null >Packages
 bzip2 Packages
